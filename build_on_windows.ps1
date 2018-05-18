460b1d8657c7f415bfd6fdd97788be14f4affc94
Christopher Dembia <cld72@cornell.edu>
2018-05-18 09:59:10 -0700

    Continue trying to fix syntax error.

--- build_on_windows.ps1
+++ build_on_windows.ps1
@@ -21,7 +21,7 @@ $OPENSIM_CORE_DEP_INSTALL_DIR = $pwd\opensim-core-dep-install
 $OPENSIM_GUI_SOURCE_DIR = $pwd\opensim-gui
 $OPENSIM_GUI_BUILD_DIR = $pwd\opensim-gui-build
 # The CMake variable JAVA_HOME tells CMake where to look for Java.
-$env:Path = "$env:JAVA_HOME\\bin;$env:Path"
+$env:Path = "$env:JAVA_HOME\bin;$env:Path"
 $env:Path = "$NSIS_DIR;$env.Path"
 
 [xml]$xml = Get-Content git-tags.xml
@@ -40,36 +40,53 @@ dir $OPENSIM_CORE_SOURCE_DIR
 ## Superbuild dependencies. 
 mkdir $OPENSIM_CORE_DEP_BUILD_DIR
 cd $OPENSIM_CORE_DEP_BUILD_DIR
-cmake $OPENSIM_CORE_DEP_SOURCE_DIR -G"$CMAKE_GENERATOR"
--T"$env:CMAKE_TOOLSET" -DCMAKE_INSTALL_PREFIX=$env:OPENSIM_CORE_DEP_INSTALL_DIR -DSUPERBUILD_simbody=ON
-- cmake --build . --config Release -- /maxcpucount:4 /verbosity:quiet
-- mkdir %OPENSIM_CORE_BUILD_DIR%
+# The backtick is line continuation, but make sure there is no whitespace
+# after the backtick!
+cmake $OPENSIM_CORE_DEP_SOURCE_DIR `
+    -G"$CMAKE_GENERATOR" `
+    -T"$CMAKE_TOOLSET" `
+    -DCMAKE_INSTALL_PREFIX=$OPENSIM_CORE_DEP_INSTALL_DIR `
+    -DSUPERBUILD_simbody=ON
+cmake --build . --config Release -- /maxcpucount:4 /verbosity:quiet
+mkdir $OPENSIM_CORE_BUILD_DIR
 ## Configure and build OpenSim.
-# Must create separate build dir, otherwise can't read test files
-# for some reason.
-- cd %OPENSIM_CORE_BUILD_DIR%
+cd $OPENSIM_CORE_BUILD_DIR
 # Configure.
 # Set the CXXFLAGS environment variable to turn warnings into errors.
-- cmake -E env CXXFLAGS="/WX" cmake %OPENSIM_CORE_SOURCE_DIR% -G"%CMAKE_GENERATOR%" -T$CMAKE_TOOLSET -DOPENSIM_DEPENDENCIES_DIR=%OPENSIM_CORE_DEP_INSTALL_DIR% -DCMAKE_INSTALL_PREFIX=%OPENSIM_CORE_INSTALL_DIR% -DBUILD_JAVA_WRAPPING=ON -DBUILD_PYTHON_WRAPPING=ON -DWITH_BTK:BOOL=ON
+cmake -E env CXXFLAGS="/WX" `
+    cmake $OPENSIM_CORE_SOURCE_DIR `
+        -G"$CMAKE_GENERATOR" `
+        -T$CMAKE_TOOLSET `
+        -DOPENSIM_DEPENDENCIES_DIR=$OPENSIM_CORE_DEP_INSTALL_DIR `
+        -DCMAKE_INSTALL_PREFIX=$OPENSIM_CORE_INSTALL_DIR `
+        -DBUILD_JAVA_WRAPPING=ON `
+        -DBUILD_PYTHON_WRAPPING=ON `
+        -DWITH_BTK:BOOL=ON
 
 # Build.
-- cmake --build . --target doxygen --config Release
-- cmake --build . --config Release -- /maxcpucount:4 /verbosity:quiet
-- cmake --build . --target install --config Release -- /maxcpucount:4 /verbosity:quiet
+cmake --build . --target doxygen --config Release
+cmake --build . --config Release -- /maxcpucount:4 /verbosity:quiet
+cmake --build . --target install --config Release -- /maxcpucount:4 /verbosity:quiet
 
 # Obtain opensim-gui source code.
-- git clone https://github.com/opensim-org/opensim-gui %OPENSIM_GUI_SOURCE_DIR%
-- cd %OPENSIM_GUI_SOURCE_DIR%
+git clone https://github.com/opensim-org/opensim-gui $OPENSIM_GUI_SOURCE_DIR
+cd $OPENSIM_GUI_SOURCE_DIR
 # TODO how to handle tags for these submodules?
-- git submodule update --init --recursive -- opensim-models opensim-visualizer Gui/opensim/threejs
+git submodule update --init --recursive -- `
+    opensim-models `
+    opensim-visualizer `
+    Gui/opensim/threejs
 
 
-  # Build opensim-gui.
-- mkdir %OPENSIM_GUI_BUILD_DIR%
-- cd %OPENSIM_GUI_BUILD_DIR%
-- cmake %OPENSIM_GUI_SOURCE_DIR% -G"Visual Studio 15 2017 Win64" -DCMAKE_PREFIX_PATH=%OPENSIM_CORE_INSTALL_DIR% -DANT_ARGS="-Dnbplatform.default.netbeans.dest.dir=C:/Program Files/NetBeans 8.2;-Dnbplatform.default.harness.dir=C:/Program Files/NetBeans 8.2/harness"
-- cmake --build . --target CopyOpenSimCore --config Release
-- cmake --build . --target CopyModels --config Release
-- cmake --build . --target PrepareInstaller --config Release
-- cmake --build . --target CopyJRE --config Release
-- cmake --build . --target CopyVisualizer --config Release
+# Build opensim-gui.
+mkdir $OPENSIM_GUI_BUILD_DIR
+cd $OPENSIM_GUI_BUILD_DIR
+cmake $OPENSIM_GUI_SOURCE_DIR `
+    -G"$CMAKE_GENERATOR" `
+    -DCMAKE_PREFIX_PATH=$OPENSIM_CORE_INSTALL_DIR `
+    -DANT_ARGS="-Dnbplatform.default.netbeans.dest.dir=C:/Program Files/NetBeans 8.2;-Dnbplatform.default.harness.dir=C:/Program Files/NetBeans 8.2/harness"
+cmake --build . --target CopyOpenSimCore --config Release
+cmake --build . --target CopyModels --config Release
+cmake --build . --target PrepareInstaller --config Release
+cmake --build . --target CopyJRE --config Release
+cmake --build . --target CopyVisualizer --config Release
