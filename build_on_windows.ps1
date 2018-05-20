8199bc32be3ee4edadb3ace65cf53b709e86cd4d
Christopher Dembia <cld72@cornell.edu>
2018-05-19 17:49:21 -0700

    Uncomment the rest of the file.

--- build_on_windows.ps1
+++ build_on_windows.ps1
@@ -34,90 +34,84 @@ $OPENSIM_GUI_GIT_TAG = $xml.info.opensim_gui_git_tag
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $OPENSIM_CORE_ZIP = "$OPENSIM_CORE_GIT_TAG.zip"
 $OPENSIM_CORE_ARCHIVE_URL = "https://github.com/opensim-org/opensim-core/archive/$OPENSIM_CORE_ZIP"
-echo "HI"
-dir
-echo "HELLO"
 (New-Object System.Net.WebClient).DownloadFile($OPENSIM_CORE_ARCHIVE_URL, "$pwd\$OPENSIM_CORE_ZIP")
-echo "AGAIN"
-dir
-echo "HEY"
-# # & "C:\Program Files\7-Zip\7z.exe" x $OPENSIM_CORE_ZIP
-# # mv opensim-core-$OPENSIM_CORE_GIT_TAG $OPENSIM_CORE_SOURCE_DIR
-# # dir $OPENSIM_CORE_SOURCE_DIR
+& "C:\Program Files\7-Zip\7z.exe" x $OPENSIM_CORE_ZIP
+mv opensim-core-$OPENSIM_CORE_GIT_TAG $OPENSIM_CORE_SOURCE_DIR
+dir $OPENSIM_CORE_SOURCE_DIR
 
-# # ## Superbuild dependencies. 
-# # # mkdir $OPENSIM_CORE_DEP_BUILD_DIR
-# # cd $OPENSIM_CORE_DEP_BUILD_DIR
-# # # The backtick is line continuation, but make sure there is no whitespace
-# # # after the backtick!
-# # cmake $OPENSIM_CORE_DEP_SOURCE_DIR `
-    # # -G"$CMAKE_GENERATOR" `
-    # # -T"$CMAKE_TOOLSET" `
-    # # -DCMAKE_INSTALL_PREFIX=$OPENSIM_CORE_DEP_INSTALL_DIR `
-    # # -DSUPERBUILD_simbody=ON
-# # cmake --build . --config Release -- /maxcpucount:4 /verbosity:quiet
-# # mkdir $OPENSIM_CORE_BUILD_DIR
-# # ## Configure and build OpenSim.
-# # cd $OPENSIM_CORE_BUILD_DIR
-# # # Configure.
-# # # Set the CXXFLAGS environment variable to turn warnings into errors.
-# # cmake -E env CXXFLAGS="/WX" `
-    # # cmake $OPENSIM_CORE_SOURCE_DIR `
-        # # -G"$CMAKE_GENERATOR" `
-        # # -T$CMAKE_TOOLSET `
-        # # -DOPENSIM_DEPENDENCIES_DIR=$OPENSIM_CORE_DEP_INSTALL_DIR `
-        # # -DCMAKE_INSTALL_PREFIX=$OPENSIM_CORE_INSTALL_DIR `
-        # # -DBUILD_JAVA_WRAPPING=ON `
-        # # -DBUILD_PYTHON_WRAPPING=ON `
-        # # -DWITH_BTK:BOOL=ON
+## Superbuild dependencies. 
+# mkdir $OPENSIM_CORE_DEP_BUILD_DIR
+cd $OPENSIM_CORE_DEP_BUILD_DIR
+# The backtick is line continuation, but make sure there is no whitespace
+# after the backtick!
+cmake $OPENSIM_CORE_DEP_SOURCE_DIR `
+    -G"$CMAKE_GENERATOR" `
+    -T"$CMAKE_TOOLSET" `
+    -DCMAKE_INSTALL_PREFIX=$OPENSIM_CORE_DEP_INSTALL_DIR `
+    -DSUPERBUILD_simbody=ON
+cmake --build . --config Release -- /maxcpucount:4 /verbosity:quiet
+mkdir $OPENSIM_CORE_BUILD_DIR
+## Configure and build OpenSim.
+cd $OPENSIM_CORE_BUILD_DIR
+# Configure.
+# Set the CXXFLAGS environment variable to turn warnings into errors.
+cmake -E env CXXFLAGS="/WX" `
+    cmake $OPENSIM_CORE_SOURCE_DIR `
+        -G"$CMAKE_GENERATOR" `
+        -T$CMAKE_TOOLSET `
+        -DOPENSIM_DEPENDENCIES_DIR=$OPENSIM_CORE_DEP_INSTALL_DIR `
+        -DCMAKE_INSTALL_PREFIX=$OPENSIM_CORE_INSTALL_DIR `
+        -DBUILD_JAVA_WRAPPING=ON `
+        -DBUILD_PYTHON_WRAPPING=ON `
+        -DWITH_BTK:BOOL=ON
 
-# # # Build.
-# # cmake --build . --target doxygen --config Release
-# # cmake --build . --config Release -- /maxcpucount:4 /verbosity:quiet
-# # cmake --build . --target install --config Release -- /maxcpucount:4 /verbosity:quiet
+# Build.
+cmake --build . --target doxygen --config Release
+cmake --build . --config Release -- /maxcpucount:4 /verbosity:quiet
+cmake --build . --target install --config Release -- /maxcpucount:4 /verbosity:quiet
 
-# # # Obtain opensim-gui source code.
-# # git clone https://github.com/opensim-org/opensim-gui $OPENSIM_GUI_SOURCE_DIR
-# # cd $OPENSIM_GUI_SOURCE_DIR
-# # # TODO how to handle tags for these submodules?
-# # git submodule update --init --recursive -- `
-    # # opensim-models `
-    # # opensim-visualizer `
-    # # Gui/opensim/threejs
+# Obtain opensim-gui source code.
+git clone https://github.com/opensim-org/opensim-gui $OPENSIM_GUI_SOURCE_DIR
+cd $OPENSIM_GUI_SOURCE_DIR
+# TODO how to handle tags for these submodules?
+git submodule update --init --recursive -- `
+    opensim-models `
+    opensim-visualizer `
+    Gui/opensim/threejs
 
 
-# # # Build opensim-gui.
-# # mkdir $OPENSIM_GUI_BUILD_DIR
-# # cd $OPENSIM_GUI_BUILD_DIR
-# # cmake $OPENSIM_GUI_SOURCE_DIR `
-    # # -G"$CMAKE_GENERATOR" `
-    # # -DCMAKE_PREFIX_PATH=$OPENSIM_CORE_INSTALL_DIR `
-    # # -DANT_ARGS="-Dnbplatform.default.netbeans.dest.dir=C:/Program Files/NetBeans 8.2;-Dnbplatform.default.harness.dir=C:/Program Files/NetBeans 8.2/harness"
-# # cmake --build . --target CopyOpenSimCore --config Release
-# # cmake --build . --target CopyModels --config Release
-# # cmake --build . --target PrepareInstaller --config Release
-# # cmake --build . --target CopyJRE --config Release
-# # cmake --build . --target CopyVisualizer --config Release
+# Build opensim-gui.
+mkdir $OPENSIM_GUI_BUILD_DIR
+cd $OPENSIM_GUI_BUILD_DIR
+cmake $OPENSIM_GUI_SOURCE_DIR `
+    -G"$CMAKE_GENERATOR" `
+    -DCMAKE_PREFIX_PATH=$OPENSIM_CORE_INSTALL_DIR `
+    -DANT_ARGS="-Dnbplatform.default.netbeans.dest.dir=C:/Program Files/NetBeans 8.2;-Dnbplatform.default.harness.dir=C:/Program Files/NetBeans 8.2/harness"
+cmake --build . --target CopyOpenSimCore --config Release
+cmake --build . --target CopyModels --config Release
+cmake --build . --target PrepareInstaller --config Release
+cmake --build . --target CopyJRE --config Release
+cmake --build . --target CopyVisualizer --config Release
 
 
-# # # Create files to distribute.
-# # mkdir $env:APPVEYOR_BUILD_FOLDER\release
-# # cd $env:APPVEYOR_BUILD_FOLDER\release
-# # # TODO use shortened git commit if not using a tag.
-# # $OPENSIM_CORE_SOURCE_ZIP = "$env:APPVEYOR_BUILD_FOLDER\release\OpenSimCore-$OPENSIM_CORE_GIT_TAG-source.zip"
-# # & "C:\Program Files\7-Zip\7z.exe" a $OPENSIM_CORE_SOURCE_ZIP $OPENSIM_CORE_INSTALL_DIR
+# Create files to distribute.
+mkdir $env:APPVEYOR_BUILD_FOLDER\release
+cd $env:APPVEYOR_BUILD_FOLDER\release
+# TODO use shortened git commit if not using a tag.
+$OPENSIM_CORE_SOURCE_ZIP = "$env:APPVEYOR_BUILD_FOLDER\release\OpenSimCore-$OPENSIM_CORE_GIT_TAG-source.zip"
+& "C:\Program Files\7-Zip\7z.exe" a $OPENSIM_CORE_SOURCE_ZIP $OPENSIM_CORE_INSTALL_DIR
 
-# # # TODO ResourceHacker. Or can NSIS set the application icon for us?
-# # # TODO Visual C++ redistributable.
-# # # https://docs.microsoft.com/en-us/cpp/ide/determining-which-dlls-to-redistribute
-# # # 
-# # cd $OPENSIM_GUI_SOURCE_DIR\Gui\opensim\dist\installer\
-# # makensis $OPENSIM_GUI_SOURCE_DIR\Gui\opensim\dist\installer\make_installer.nsi
-# # mv $OPENSIM_GUI_SOURCE_DIR\Gui\opensim\dist\installer\*.exe `
-    # # $env:APPVEYOR_BUILD_FOLDER\release\
+# TODO ResourceHacker. Or can NSIS set the application icon for us?
+# TODO Visual C++ redistributable.
+# https://docs.microsoft.com/en-us/cpp/ide/determining-which-dlls-to-redistribute
+# 
+cd $OPENSIM_GUI_SOURCE_DIR\Gui\opensim\dist\installer\
+makensis $OPENSIM_GUI_SOURCE_DIR\Gui\opensim\dist\installer\make_installer.nsi
+mv $OPENSIM_GUI_SOURCE_DIR\Gui\opensim\dist\installer\*.exe `
+    $env:APPVEYOR_BUILD_FOLDER\release\
 
-# # $OPENSIM_GUI_ZIP = "$env:APPVEYOR_BUILD_FOLDER\release\OpenSim-$OPENSIM_GUI_GIT_TAG.zip"
-# # & "C:\Program Files\7-Zip\7z.exe" a "$OPENSIM_GUI_ZIP" "$OPENSIM_GUI_SOURCE_DIR\Gui\opensim\dist\installer\opensim\"
+$OPENSIM_GUI_ZIP = "$env:APPVEYOR_BUILD_FOLDER\release\OpenSim-$OPENSIM_GUI_GIT_TAG.zip"
+& "C:\Program Files\7-Zip\7z.exe" a "$OPENSIM_GUI_ZIP" "$OPENSIM_GUI_SOURCE_DIR\Gui\opensim\dist\installer\opensim\"
 
 
 
